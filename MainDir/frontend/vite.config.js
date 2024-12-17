2f4f75dce58becf5da8f27e1dbc58b8f67fb3726
pushpak-angularminds <pushpak@angularminds.in>
2024-12-17 15:54:22 +0530

    Add GitHub Actions workflow for deployment

--- MainDir/frontend/vite.config.js
+++ MainDir/frontend/vite.config.js
@@ -1,20 +1,13 @@
-// import { defineConfig } from 'vite'
-// import react from '@vitejs/plugin-react'
-
-// // https://vite.dev/config/
-// export default defineConfig({
-//   plugins: [react()],
-// })
-
-import path from "path"
-import react from "@vitejs/plugin-react"
-import { defineConfig } from "vite"
+import path from "path";
+import react from "@vitejs/plugin-react";
+import { defineConfig } from "vite";
 
 export default defineConfig({
   plugins: [react()],
   resolve: {
     alias: {
-      "@": path.resolve(__dirname, "./src"),
+      "@": path.resolve(__dirname, "src"), // Resolves the '@' alias to the 'src' folder
     },
   },
-})
+  base: '/FormCraft/', // Ensure the base path matches your GitHub repository name for deployment
+});
